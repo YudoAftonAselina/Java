c88464877235281a024a8ddd24ea7a7ad7f3c68a
YudoAftonAselina <Yudoharunwardhana0821@gmail.com>
2025-10-22 14:39:24 +0700

    Menambahkan file latihan ke 8

---
+++ Learning-Java/Latihan/Latihan8.java
@@ -0,0 +1,36 @@
+package Latihan;
+
+import java.util.Scanner;
+
+public class Latihan8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x, random, hearth;
+        random = (int) (Math.random() * 10);
+        hearth = 5;
+
+        System.out.println("-- Permainan Tebak Angka --");
+        while (true) {
+            if (hearth > 0) {
+                System.out.print("Nyawa kamu: " + hearth + "\nMasukan angka 1 - 10: ");
+                x = input.nextInt();
+
+                if (x == random) {
+                    System.out.print("Game Over: Selamat kamu berhasil");
+                    break;
+                } else if (x < random) {
+                    System.out.println("\nMessage: Angka terlalu kecil");
+                } else {
+                    System.out.println("\nMessage: Angka terlalu besar");
+                }
+
+            } else {
+                System.out.print("Game Over: Kamu kehabisan nyawa huhu...");
+                break;
+            }
+
+            hearth -= 1;
+        }
+    }
+}
